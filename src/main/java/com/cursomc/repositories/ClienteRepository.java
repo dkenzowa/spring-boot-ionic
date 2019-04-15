c3f804d1a63861f2da22d2a544374543df065adc
Danilo Kenzo <dani.kenzo@hotmail.com>
2019-04-15 11:31:55 -0300

    Validação customizada email não repetido na inserção do cliente

--- src/main/java/com/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/cursomc/repositories/ClienteRepository.java
@@ -8,4 +8,6 @@ import com.cursomc.domain.Cliente;
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
+	Cliente findByEmail(String email);
+	
 }
--- src/main/java/com/cursomc/services/validation/ClienteInsertValidation.java
+++ src/main/java/com/cursomc/services/validation/ClienteInsertValidation.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.cursomc.domain.Cliente;
 import com.cursomc.domain.enums.TipoCliente;
 import com.cursomc.dto.ClienteNewDTO;
+import com.cursomc.repositories.ClienteRepository;
 import com.cursomc.resources.exception.FieldMessage;
 import com.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidation implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -20,14 +28,19 @@ public class ClienteInsertValidation implements ConstraintValidator<ClienteInser
 	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
 		List<FieldMessage> list = new ArrayList<>();
 
-		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CPF inválido"));
 		}
-		
-		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
 		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
